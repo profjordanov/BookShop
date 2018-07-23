e4fbb7050875303a8430618e036551077a10ecf2
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-23 19:50:30 +0300

    Authors Controller added

---
+++ src/server/BookShop.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Authors")]
+    public class AuthorsController : ApiController
+    {
+    }
+}
