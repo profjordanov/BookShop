baa835918defc99e98040753b68bf227da4a47c3
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-23 19:42:24 +0300

    migrations and app context

--- src/server/BookShop.Data.EntityFramework/ApplicationDbContext.cs
+++ src/server/BookShop.Data.EntityFramework/ApplicationDbContext.cs
@@ -11,5 +11,40 @@ namespace BookShop.Data.EntityFramework
             : base (options)
         {
         }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<CategoryBook> CategoryBooks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<CategoryBook>()
+                .HasKey(cb => new { cb.CategoryId, cb.BookId });
+
+            builder
+                .Entity<CategoryBook>()
+                .HasOne(cb => cb.Category)
+                .WithMany(c => c.Books)
+                .HasForeignKey(cb => cb.CategoryId);
+
+            builder
+                .Entity<CategoryBook>()
+                .HasOne(cb => cb.Book)
+                .WithMany(b => b.Categories)
+                .HasForeignKey(cb => cb.BookId);
+
+            builder
+                .Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId);
+
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ src/server/BookShop.Data.EntityFramework/Migrations/20180723164003_AddModels.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookShop.Data.EntityFramework.Migrations
+{
+    public partial class AddModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
