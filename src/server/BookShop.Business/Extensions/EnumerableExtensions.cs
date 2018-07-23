53b21c8ee28afbcce152268594ecfde81d3e0c49
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-23 19:45:08 +0300

    EnumerableExtensions added

---
+++ src/server/BookShop.Business/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BookShop.Business.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static ISet<T> ToHashSet<T>(this IEnumerable<T> enumerable)
+            => new HashSet<T>(enumerable);
+    }
+}
