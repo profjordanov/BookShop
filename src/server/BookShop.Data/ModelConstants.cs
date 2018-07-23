258d53d787ad303be3edfdb4c183b97435fc7525
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-23 19:11:59 +0300

    Model Constants class added

---
+++ src/server/BookShop.Data/ModelConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Data
+{
+    public class ModelConstants
+    {
+        public const int BookTitleMinLength = 2;
+        public const int BookTitleMaxLength = 100;
+
+        public const int AuthorNameMaxLength = 50;
+
+        public const int CategoryNameMaxLength = 50;
+    }
+}
